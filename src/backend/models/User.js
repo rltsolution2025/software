8b1f76eed7dc0ed2071fb24636810d7e91a90da1
flora200198 <aflora398@gmail.com>
2025-11-18 09:32:25 +0530

    PO email generation upated

--- src/backend/models/User.js
+++ src/backend/models/User.js
@@ -1,13 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const userSchema = new mongoose.Schema({
-//   username: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-//   role: { type: String, enum: ['admin', 'user'], default: 'user' },
-// });
-
-// module.exports = mongoose.model('User', userSchema);
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
