510fa251f776ecd88ae572cea9a0353a462bee35
flora200198 <aflora398@gmail.com>
2025-11-26 15:54:48 +0530

    login credential updated

--- src/backend/createUser.js
+++ src/backend/createUser.js
@@ -3,7 +3,12 @@ const bcrypt = require("bcryptjs");
 const User = require("./models/User"); // <-- path to your User model
 
 async function createAdmin() {
-  await mongoose.connect("mongodb://localhost:27017/yourdbname"); // change db name if needed
+  // await mongoose.connect("mongodb://localhost:27017/yourdbname"); // change db name if needed
+  await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/erp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
 
   const hashedPassword = await bcrypt.hash("adminpass", 10);
 
--- src/backend/routes/authRoute.js
+++ src/backend/routes/authRoute.js
@@ -1,39 +1,3 @@
-// const express = require("express");
-// const bcrypt = require("bcryptjs");
-// const User = require("../models/User");
-// const { login, logout } = require("../controllers/authController");
-
-// const router = express.Router();
-
-// router.get("/create-admin", async (req, res) => {
-//   try {
-//     const existingAdmin = await User.findOne({ username: "admin" });
-
-//     if (existingAdmin) {
-//       return res.status(400).json({ message: "Admin already exists" });
-//     }
-
-//     const hashedPassword = await bcrypt.hash("adminpass", 10);
-
-//     await User.create({
-//       username: "admin",
-//       password: hashedPassword,
-//       role: "admin",
-//     });
-
-//     res.send("✅ Admin Created Successfully");
-//   } catch (error) {
-//     res.status(500).json({ message: "Error creating admin", error });
-//   }
-// });
-
-
-// router.post("/login", login);
-// router.post("/logout", logout);
-
-// module.exports = router;
-
-
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
