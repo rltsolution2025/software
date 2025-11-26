a9975e7cb7e2f343ab25cec93fb98fff217c4ff4
flora200198 <aflora398@gmail.com>
2025-11-26 16:30:41 +0530

    backend dashboard route updated

--- src/backend/controllers/authController.js
+++ src/backend/controllers/authController.js
@@ -30,6 +30,7 @@ const login = async (req, res) => {
       {
         id: user._id,
         role: user.role,
+        username: user.username
       },
       process.env.JWT_SECRET || "secret123",
       { expiresIn: "7d" }
